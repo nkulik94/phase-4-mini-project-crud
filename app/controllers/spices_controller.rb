f86223e0cfa0233ceb9d8e0cc9852a68298433d2
Naftali Kulik <nkulik1994@gmail.com>
2022-07-12 15:02:55 -0400

    commit tree 11597492f2674d938a3631dde5161c7ee9bac802

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,34 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Spice.all
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find(params[:id])
+        spice.update(spice_params)
+        render json: spice, status: :accepted
+    end
+
+    def destroy
+        spice = Spice.find(params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found_response
+        render json: { error: "Spice not found" }, status: :not_found
+    end
+end
